ee509a445227906e1aba30a08f33587169ed452a
Hwarang <qubic98@gmail.com>
2020-07-31 05:38:37 +0900

    beta-0731 savaApplicationLog bug fixed

--- app/src/main/java/kr/ac/inha/mindscope/Tools.java
+++ app/src/main/java/kr/ac/inha/mindscope/Tools.java
@@ -742,7 +742,7 @@ public class Tools {
             SharedPreferences prefs = con.getSharedPreferences("Configurations", Context.MODE_PRIVATE);
             int dataSourceId = prefs.getInt("APPLICATION_LOG", -1);
             assert dataSourceId != -1;
-            if(uniqueTagForEachActivityOrEvent != null || action != null || sb != null)
+            if(uniqueTagForEachActivityOrEvent != null && action != null && sb != null)
                 DbMgr.saveMixedData(dataSourceId, timestamp, 1.0f, timestamp, logDate, uniqueTagForEachActivityOrEvent, action, sb.toString());
         }).start();
     }
