3a39336d6c3f942858e03c9ac10aedad77701f65
[jay] <[rurusu0704@gmail.com]>
2021-01-26 20:11:30 +0900

    20.01.26 / junyoung / 2021-01-27 부터 컨디션3-2-1 순으로 진행

--- app/src/main/java/kr/ac/inha/mindscope/Tools.java
+++ app/src/main/java/kr/ac/inha/mindscope/Tools.java
@@ -86,11 +86,11 @@ public class Tools {
     public static final int CATEGORY_FOOD_APP_USAGE = 27;
     public static final long STEP0_EXPIRE_TIMESTAMP_VALUE = 60 * 60 * 24 * 0 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 50 * 1 * 1000;  // step0 이제 쓰이지 않음
     //todo 2-2-2 적용
-    public static final long STEP1_EXPIRE_TIMESTAMP_VALUE = 60 * 60 * 24 * 10 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 55 * 1 * 1000;  //todo STEP1 만료기간
-    public static final long CONDITION_EXPIRE_DURATION1 = 60 * 60 * 24 * 10 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 50 * 1 * 1000; //todo condition 기간 변경은 맨 앞에 60 * 60 * 24 * 날짜 * 1000에서 날짜부분 변경
-    public static final long CONDITION_EXPIRE_DURATION2 = 60 * 60 * 24 * 13 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 50 * 1 * 1000;
-    public static final long CONDITION_EXPIRE_DURATION3 = 60 * 60 * 24 * 16 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 50 * 1 * 1000;
-    public static final long CONDITION_EXPIRE_DURATION4 = 60 * 60 * 24 * 19 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 50 * 1 * 1000; //condition 끝나는 기간 = 실험 종료 기간을 나타낸 거지만 쓰이지 않음
+    public static final long STEP1_EXPIRE_TIMESTAMP_VALUE = 60 * 60 * 24 * 1 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 55 * 1 * 1000;  //todo STEP1 만료기간
+    public static final long CONDITION_EXPIRE_DURATION1 = 60 * 60 * 24 * 1 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 50 * 1 * 1000; //todo condition 기간 변경은 맨 앞에 60 * 60 * 24 * 날짜 * 1000에서 날짜부분 변경
+    public static final long CONDITION_EXPIRE_DURATION2 = 60 * 60 * 24 * 3 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 50 * 1 * 1000;
+    public static final long CONDITION_EXPIRE_DURATION3 = 60 * 60 * 24 * 5 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 50 * 1 * 1000;
+    public static final long CONDITION_EXPIRE_DURATION4 = 60 * 60 * 24 * 7 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 50 * 1 * 1000; //condition 끝나는 기간 = 실험 종료 기간을 나타낸 거지만 쓰이지 않음
 
 //    public static final long STEP1_EXPIRE_TIMESTAMP_VALUE = 60 * 60 * 24 * 6 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 55 * 1 * 1000;  //todo STEP1 만료기간
 //    public static final long CONDITION_EXPIRE_DURATION1 = 60 * 60 * 24 * 6 * 1000 + 60 * 60 * 10 * 1 * 1000 + 60 * 50 * 1 * 1000; //todo condition 기간 변경은 맨 앞에 60 * 60 * 24 * 날짜 * 1000에서 날짜부분 변경
@@ -947,6 +947,9 @@ public class Tools {
         }
         int stepCheck = stepChangePrefs.getInt("stepCheck", 0);
 
+        //2021-01-27으로 condition3으로 시작을 위해 조인날짜 고정
+        String join = "1611586800000";
+        joinTimestamp = Long.parseLong(join);
         long diff = curTimestamp - joinTimestamp;
         SharedPreferences.Editor stepEditor = stepChangePrefs.edit();
 //        if (diff >= STEP0_EXPIRE_TIMESTAMP_VALUE && diff < STEP1_EXPIRE_TIMESTAMP_VALUE) { // 이전에는 step0가 존재했지만 이젠 존재 x
